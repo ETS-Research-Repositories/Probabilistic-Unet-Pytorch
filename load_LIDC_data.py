941bb5d4e016cd031ef6a20d60ebac0f91073f01
jizong <jizong.peng.1@etsmtl.net>
2020-03-09 23:29:38 -0400

    black formatter

--- load_LIDC_data.py
+++ load_LIDC_data.py
@@ -1,10 +1,11 @@
+import os
+import pickle
+import random
+
+import numpy as np
 import torch
 from torch.utils.data.dataset import Dataset
-from torch.utils.data import DataLoader
-import numpy as np
-import os
-import random
-import pickle
+
 
 class LIDC_IDRI(Dataset):
     images = []
@@ -13,27 +14,27 @@ class LIDC_IDRI(Dataset):
 
     def __init__(self, dataset_location, transform=None):
         self.transform = transform
-        max_bytes = 2**31 - 1
+        max_bytes = 2 ** 31 - 1
         data = {}
         for file in os.listdir(dataset_location):
             filename = os.fsdecode(file)
-            if '.pickle' in filename:
+            if ".pickle" in filename:
                 print("Loading file", filename)
                 file_path = dataset_location + filename
                 bytes_in = bytearray(0)
                 input_size = os.path.getsize(file_path)
-                with open(file_path, 'rb') as f_in:
+                with open(file_path, "rb") as f_in:
                     for _ in range(0, input_size, max_bytes):
                         bytes_in += f_in.read(max_bytes)
                 new_data = pickle.loads(bytes_in)
                 data.update(new_data)
-        
-        for key, value in data.items():
-            self.images.append(value['image'].astype(float))
-            self.labels.append(value['masks'])
-            self.series_uid.append(value['series_uid'])
 
-        assert (len(self.images) == len(self.labels) == len(self.series_uid))
+        for key, value in data.items():
+            self.images.append(value["image"].astype(float))
+            self.labels.append(value["masks"])
+            self.series_uid.append(value["series_uid"])
+
+        assert len(self.images) == len(self.labels) == len(self.series_uid)
 
         for img in self.images:
             assert np.max(img) <= 1 and np.min(img) >= 0
@@ -46,8 +47,8 @@ class LIDC_IDRI(Dataset):
     def __getitem__(self, index):
         image = np.expand_dims(self.images[index], axis=0)
 
-        #Randomly select one of the four labels for this image
-        label = self.labels[index][random.randint(0,3)].astype(float)
+        # Randomly select one of the four labels for this image
+        label = self.labels[index][random.randint(0, 3)].astype(float)
         if self.transform is not None:
             image = self.transform(image)
 
@@ -57,7 +58,7 @@ class LIDC_IDRI(Dataset):
         image = torch.from_numpy(image)
         label = torch.from_numpy(label)
 
-        #Convert uint8 to float tensors
+        # Convert uint8 to float tensors
         image = image.type(torch.FloatTensor)
         label = label.type(torch.FloatTensor)
 
